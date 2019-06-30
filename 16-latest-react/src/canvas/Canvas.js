ab10fa9787b6428079ac0e2cf8f29037f5e38bed
Brennan Cheung <git@brennancheung.com>
2019-06-29 23:54:41 -0700

    drag drop WIP

--- 16-latest-react/src/canvas/Canvas.js
+++ 16-latest-react/src/canvas/Canvas.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Transistor from '../parts/Transistor'
+
+export const CanvasContext = React.createContext()
 
 const Canvas = () => {
+  const canvasRef = useRef()
   return (
     <div className="canvas">
       <h3>Canvas</h3>
+      <CanvasContext.Provider value={canvasRef}>
+        <svg
+          ref={canvasRef}
+          width="600"
+          height="600"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <Transistor />
+        </svg>
+      </CanvasContext.Provider>
     </div>
   )
 }
--- 16-latest-react/src/parts/Transistor.js
+++ 16-latest-react/src/parts/Transistor.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import PropTypes from 'prop-types'
+import { CanvasContext } from '../canvas/Canvas'
 
-const Transistor = () => {
+// <img src="/images/parts/transistor.png" alt="" width="100" height="100" />
+//
+const Transistor = ({ x = 50, y = 50, r = 10 }) => {
+  const canvasRef = useContext(CanvasContext)
+  const [dragging, setDragging] = useState(false)
+  const [coord, setCoord] = useState({ x, y })
+  const [offset, setOffset] = useState({ x: 0, y: 0 })
+  const [origin, setOrigin] = useState({ x: 0, y: 0 })
+
+  const xPos = coord.x + offset.x
+  const yPos = coord.y + offset.y
+
+  const transform = `translate(${xPos}, ${yPos})`
+
+  const fill = dragging ? 'red' : 'green'
+  const stroke = 'black'
+
+  const handleMouseDown = e => {
+    setDragging(true)
+    setOrigin({ x: e.clientX, y: e.clientY })
+  }
+
+  const handleMouseMove = e => {
+    if (!dragging) { return }
+    setOffset({
+      x: e.clientX - origin.x,
+      y: e.clientY - origin.y,
+    })
+  }
+
+  const handleMouseUp = e => {
+    setDragging(false)
+    setCoord({ x: xPos, y: yPos })
+    setOrigin({ x: 0, y: 0 })
+    setOffset({ x: 0, y: 0 })
+  }
+
   return (
-    <div className="part transistor">
-      <img src="/images/parts/transistor.png" alt="" width="100" height="100" />
-    </div>
+    <svg style={{ userSelect: 'none' }}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseUp}
+    >
+      <circle transform={transform} cx="0" cy="0" r={r} fill={fill} stroke={stroke} />
+    </svg>
   )
+}
+
+Transistor.propTypes = {
+  x: PropTypes.number,
+  y: PropTypes.number,
+  r: PropTypes.number,
 }
 
 export default Transistor
